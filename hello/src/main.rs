0444684a6730617ddbaa9048e6f66eba1d636ce7
root <root@silviaseDESTKTOP.localdomain>
2020-08-25 12:02:54 +0900

    hirumeshi mae no commit

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -20,8 +20,8 @@ fn main() {
     // 文字列型
     let s = "This is it. That is that that that that boy said."; // 文字列スライス 参照のみが出来る
     let str = String::from("This is String Type");
-    println!("slice :{}", s);
-    println!("string :{}", str);
+    println!("slice  : {}", s);
+    println!("string : {}", str);
 
     // ユーザー定義型
     let lolico = TraPMember {
@@ -33,5 +33,41 @@ fn main() {
     println!(
         "lolico's ID -> {}, username -> {}, grade -> {}",
         lolico.id, lolico.username, lolico.grade
-    )
+    );
+
+    x = -3;
+    // Rustにおいて, ifは文ではなく式なので評価が出来る
+    let abs_x = if x >= 0 { x } else { -x };
+    println!("abs(x) -> {}", abs_x);
+
+    // loop は式なので値を返すことが出来る
+    let mut hogehoge = 12345678901234567890u64;
+    let mut count = 0;
+    let res = loop {
+        hogehoge /= 2;
+        count += 1;
+        if hogehoge == 0 {
+            break count;
+        }
+    };
+    println!("hogehoge : {} bit", res);
+
+    'one: loop {
+        'two: loop {
+            'three: loop {
+                println!("three");
+                if res == 64 {
+                    break 'two;
+                } else if res == 63 {
+                    break 'three;
+                } else {
+                    break 'one;
+                }
+            }
+            println!("two");
+        }
+        println!("one");
+        break;
+    }
+    // three -> one になる
 }
