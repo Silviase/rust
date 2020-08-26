053aa45aec022c3906807d5b57c99f4ffc39db10
root <root@silviaseDESTKTOP.localdomain>
2020-08-26 10:47:15 +0900

    d

--- atcoder/abc170/src/bin/d.rs
+++ atcoder/abc170/src/bin/d.rs
@@ -1,3 +1,29 @@
+use proconio::input;
 fn main() {
-    unimplemented!();
+    input! {
+        n:isize,
+        mut ar: [usize; n]
+    }
+
+    ar.sort();
+    let mut num = [0; 1_000_001];
+    for x in ar {
+        num[x] += 1;
+    }
+
+    let mut res = 0;
+
+    for i in 1..=1_000_000 {
+        if num[i] == 0 {
+            continue;
+        }
+        if num[i] == 1 {
+            res += 1;
+        }
+
+        for j in (0..=1_000_000).step_by(i) {
+            num[j] = 0;
+        }
+    }
+    println!("{}", res);
 }
---
+++ atcoder/abc170/target/debug/d.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/silviase/rust/atcoder/abc170/target/debug/d: /mnt/c/Users/silviase/rust/atcoder/abc170/src/bin/d.rs
